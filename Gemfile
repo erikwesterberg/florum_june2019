bef27ed1a6b1aee6bdc569bf635008be9a2c1afe
erka <erik.westerberg@hotmail.se>
2019-07-19 11:35:47 +0200

    creates dummy data for app, 6loop for user working

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 gem 'devise'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 gem 'rails', '~> 5.2.3'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+6.times do |n|
+    name = Faker::Name.name
+    email = Faker::Internet.email
+    password = "password"
+        User.create!(
+        name: name,
+        email: email,
+        password: password,
+        password_confirmation: password)
+    puts "created user #{n+1}"
+    end
+
+    users = User.all
+
+6.times do |n|
+    title = Faker::Title.title
+    text = Faker::Text.text
+  
+    User.first.post.create(
+      title: text,
+      text: text)
+    puts "created post #{n+1}"
+    end
+
+    posts = Post.all
+
+#   6.times do
+
+#         name = Faker::Name.name
+#         details = Faker::Details.details
+#         price = Faker::Price.price
+#         category = Faker::Category.category
+#         quantity = Faker::Quantity.quantity
+
+#     User.last.plants.create(
+#       name: name,
+#       details: details,
+#       price: price,
+#       category: category,
+#       quantity: quantity
+#     )
+#   end
+  
